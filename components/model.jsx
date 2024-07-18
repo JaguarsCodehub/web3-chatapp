9581ce39f6b8fc79ac222c8f9c2cefcc456aefcc
Jyotindra Tavanoji <tavanojijyotindra@gmail.com>
2024-07-18 20:46:18 +0530

    Feature: Minor Changes

--- components/model.jsx
+++ components/model.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import Image from 'next/image';
 
-const Model = () => {
-  return <div>Model</div>;
+import images from '../assets';
+import { ChatAppContext } from '@/context/ChatAppContext';
+import Loader from './loader';
+
+const Model = ({
+  openModel,
+  title,
+  head,
+  info,
+  smallInfo,
+  image,
+  functionName,
+}) => {
+  // useState
+  const [name, setName] = useState('');
+  const [accountAddress, setAccountAddress] = useState('');
+
+  const { loading } = useContext(ChatAppContext);
+
+  return (
+    <div className='model'>
+      <div className='model_box'>
+        <div className='model_box_left'>
+          <Image src={image} alt='image' width={700} height={700} />
+        </div>
+        <div className='model_box_right'>
+          <h1>
+            {title} <span>{head}</span>
+          </h1>
+          <p>{info}</p>
+          <small>{smallInfo}</small>
+
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Model;
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,6 +1,10 @@
 import React, { useContext, useState } from 'react';
 import { ChatAppContext } from '@/context/ChatAppContext';
 import images from '../assets';
+import Image from 'next/image';
+import Link from 'next/link';
+import Model from './model';
+import Error from './error';
 
 const Navbar = () => {
   const menuItems = [
@@ -31,17 +35,17 @@ const Navbar = () => {
   ];
 
   const [active, setActive] = useState(2);
-  const [open, setOpen] = useState(false)
-  const [openModel, setOpenModel] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [openModel, setOpenModel] = useState(false);
 
-  const [account, userName, connectWallet] = useContext(ChatAppContext)
-
+  const [account, createAccount, userName, connectWallet, error] =
+    useContext(ChatAppContext);
 
   return (
     <div className=''>
       <div className=''>
         <div className=''>
-          <Image src={images.logo} alt="logo" width={50} height={50} />
+          <Image src={images.logo} alt='logo' width={50} height={50} />
         </div>
         <div className=''>
           {/* Desktop */}
@@ -49,10 +53,7 @@ const Navbar = () => {
             {menuItems.map((el, i) => (
               <div onClick={() => setActive(i + 1)} key={1 + 1} className=''>
                 {/* TODO: Add iternary strings for active on className 1:08:22*/}
-                <Link
-                  className=""
-                  href={el.link}
-                >
+                <Link className='' href={el.link}>
                   {el.menu}
                 </Link>
               </div>
@@ -60,64 +61,76 @@ const Navbar = () => {
           </div>
 
           {/* Mobile */}
-          {open && (
+          {!open && (
             <div className='mobile_menu'>
               {menuItems.map((el, i) => (
-                <div onClick={() => setActive(i + 1)} key={1 + 1} className='mobile_menu_items'>
+                <div
+                  onClick={() => setActive(i + 1)}
+                  key={1 + 1}
+                  className='mobile_menu_items'
+                >
                   {/* TODO: Add iternary strings for active on className 1:08:22*/}
-                  <Link
-                    className="mobile_menu_items_link"
-                    href={el.link}
-                  >
+                  <Link className='mobile_menu_items_link' href={el.link}>
                     {el.menu}
                   </Link>
                 </div>
               ))}
 
-
               <p className='mobile_menu_btn'>
                 <Image
-                  src={images.close} alt="close" width={50} height={50} onClick={() => setOpen(false)}
+                  src={images.close}
+                  alt='close'
+                  width={50}
+                  height={50}
+                  onClick={() => setOpen(false)}
                 />
               </p>
             </div>
           )}
 
-
           {/* Connect Wallet */}
           <div className='navbar_box_right_container'>
-            {account == "" ? (
+            {account == '' ? (
               <button onClick={() => connectWallet()}>
-                {""}
+                {''}
                 <span>Connect Wallet</span>
               </button>
             ) : (
               <button onClick={() => setOpenModel(true)}>
-                {""}
+                {''}
                 <Image
-                  src={userName ? images.accountName : images.create2} alt="image" width={20} height={20}
+                  src={userName ? images.accountName : images.create2}
+                  alt='image'
+                  width={20}
+                  height={20}
                 />
-                {""}
-                <small>
-                  {userName || "Create Acccount"}
-                </small>
+                {''}
+                <small>{userName || 'Create Acccount'}</small>
               </button>
             )}
           </div>
 
           <div className='navbar_box_right_open' onClick={() => setOpen(true)}>
-            <Image
-              src={images.open} alt="open" width={30} height={30}
-            />
+            <Image src={images.open} alt='open' width={30} height={30} />
           </div>
         </div>
       </div>
 
-
       {/* Model COmponent */}
-      {openModal && (
-        <div></div>
+      {!openModel && (
+        <div className='modelBox'>
+          <Model
+            openModel={setOpenModel}
+            title='Welcome to'
+            head='Chat Buddy'
+            info="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            smallInfo='Kindly select your name...'
+            image={images.hero}
+            functionName={createAccount}
+          />
+        </div>
       )}
+      {error == '' ? '' : <Error error />}
     </div>
   );
 };
